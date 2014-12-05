4830e4c108cb7621304094c2e72e6d6e0e11c173
QuarK264 <alexey.danilov@hotmail.com>
2014-12-06 01:28:46 +0300

    2 курс, Технология программирования, Работа с файлами

---
+++ DAA.TP.lab5/DAA.TP.lab5/FileSearch.cs
@@ -0,0 +1,46 @@
+﻿namespace DAA.TP.lab5
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.IO;
+
+    static class FileSearch
+    {
+        public static string CheckPath()
+        {
+            DirectoryInfo Path = new DirectoryInfo(@"C:\Users\1\Documents\2 курс 3 семестр - Технология программирования\Лаб5\");
+            if (Path.ToString().Length <= 32767)
+            {
+                if (Path.Exists)
+                {
+                    return Path.ToString();
+                }
+                else
+                {
+                    throw new DirectoryNotFoundException();
+                }
+            }
+            else
+            {
+                throw new PathTooLongException();
+            }
+        }
+
+        public static string CheckFile(string Path)
+        {
+            string FileName = "text.txt";
+            FileInfo File = new FileInfo(Path + FileName);
+            if (File.Exists)
+            {
+                return File.ToString();
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+        }
+    }
+}
---
+++ DAA.TP.lab5/DAA.TP.lab5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DAA.TP.lab5
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string Path = FileSearch.CheckPath();
+                string FilePath = FileSearch.CheckFile(Path);
+                Console.WriteLine(FilePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
