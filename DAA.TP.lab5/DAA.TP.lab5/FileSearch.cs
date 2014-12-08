5468351ed55ff1c44bdcc2cbbcc861e6550c80e2
QuarK264 <alexey.danilov@hotmail.com>
2014-12-08 22:42:35 +0300

    Начал работу с файлом.

--- DAA.TP.lab5/DAA.TP.lab5/FileSearch.cs
+++ DAA.TP.lab5/DAA.TP.lab5/FileSearch.cs
@@ -2,10 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
-    using System.IO;
 
     static class FileSearch
     {
---
+++ DAA.TP.lab5/DAA.TP.lab5/SearchInFile.cs
@@ -0,0 +1,21 @@
+﻿namespace DAA.TP.lab5
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    class SearchInFile
+    {
+        public static List<string> CreateStream(string FilePath)
+        {
+            StreamReader IncomingFile = new StreamReader(FilePath, Encoding.Unicode);
+            StringBuilder Text = new StringBuilder(IncomingFile.ToString());
+            List<string> ListText = new List<string>();
+            while (Text != null)
+            {
+                
+            }
+        }
+    }
+}
